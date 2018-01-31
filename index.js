d9a72db267538321068e888d519fce1e451bb08f
NihatJS <34793252+NihatJS@users.noreply.github.com>
2018-01-31 18:53:50 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
+const Discord = require("discord.js");
+const client = new Discord.Client();
 
+client.on('ready', () => {
+  console.log(`Logged in as ${client.user.tag}!`);
+});
+
+client.on('message', msg => {
+  if (msg.content === 'ping') {
+    msg.reply('Pong!');
+  }
+});
+
+client.login('NDAyNDU4NTY3NTU2MzMzNTcw.DVN33w.yCXy5HTKRplVYQGRgLVzhyAq-QY');
